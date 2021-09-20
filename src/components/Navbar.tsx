4eeee37f1ab34e65e1c75f168ef62eea642bbeb9
pathel <polegend@stud.ntnu.no>
2021-09-20 02:06:27 +0200

    add navbar components add image logo for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import image from '../img/imagetrans.jpg'
 
 export function Navbar() {
   return (
-    <nav className="self-start grid grid-rows-1 py-4">
-      <div>
-        <a href="/">Home</a>
+    <nav className="bg-gray-50 dark:bg-gray-800 border-gray-400 flex fixed md:py-5 w-screen justify-between items-center border-solid border box-border shadow-lg">
+      <div className="flex">
+        <img className="md:ml-80" src={image}/>
+        <a className="text-gray-900 dark:text-white ml-12 mt-8" href="/">Home</a>
       </div>
     </nav>
   )
