92b98744f94b6dd8161e873cb21a638b5f5ddb83
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-10-01 12:48:48 +0200

    Added basic WebStorage

---
+++ src/components/WebStorage.tsx
@@ -0,0 +1,31 @@
+import { APILoader } from "../APILoader";
+import React from "react";
+import IssueList from '../content/IssueList'
+
+interface IContentState {
+    loader: APILoader
+  }
+  
+export class Content extends React.Component<Record<string, unknown>, IContentState> {
+  
+    state: IContentState = {
+      loader: new APILoader()
+    }
+
+    saveToSessionStorage() {
+      const loader: APILoader = this.state.loader
+      const issueList: IssueList = new IssueList({loader})
+      window.sessionStorage.setItem('issueList', JSON.stringify(issueList))
+    }
+
+    loadFromSessionStorage() {
+      const issueListJSON = window.sessionStorage.getItem('issueList')
+      if (issueListJSON == null) {
+        return null;
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const issueList: IssueList = JSON.parse(issueListJSON)
+      return issueList
+    }
+
+  }
