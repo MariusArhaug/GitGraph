12ee4c6f60c92cec55ad611c94971822e693a1c9
pathel <polegend@stud.ntnu.no>
2021-10-02 15:17:36 +0200

    fix navbar blank space

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,13 @@ import image from '../img/GitGraph.svg'
 export function Navbar() {
   return (
     <nav
-      className="bg-bestGray-dark flex justify-around items-center shadow-lg w-screen"
+      className="bg-bestGray-dark flex justify-around items-center shadow-lg md:flex-shrink"
       data-testid="nav"
     >
       <img className="h-10" src={image} />
       <div className="whitespace-nowrap flex">
         <a className="mx-12 p-3 block " href="/">
           Home
-        </a>
-        <a className="mx-12 p-3 block " href="/">
-          Graphs
-        </a>
-        <a className="mx-12 p-3 block " href="/">
-          About Us
         </a>
       </div>
     </nav>
